d3c9e210588bebca076f7984c833c59e9697c0cd
Alvin Lim <alim459@gmail.com>
2016-03-31 03:48:38 -0700

    implemented brewery page

--- app/breweryuser.js
+++ app/breweryuser.js
@@ -15,7 +15,6 @@ app.controller('SearchCtrl', function($scope, $http, $timeout, $rootScope, $mdDi
     $scope.abvCategories = ["<4%", "4-4.99%", "5-5.99%", "6-6.99%", ">7%" ];
     $scope.ibuCategories = ["<10", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", ">70"];
     $scope.ratingCategories = ["1", "2", "3", "4"];
-    $rootScope.searchResults = [];
 
     $scope.submitBeer = function(ev){
     	if (!validSubmission()) {
@@ -80,7 +79,7 @@ app.controller('SearchCtrl', function($scope, $http, $timeout, $rootScope, $mdDi
     }
 
 });
-var selectedBeer = {};
+// var selectedBeer = {};
 app.controller('UpdateCtrl', function($scope, $http, $timeout, $rootScope, $mdDialog, $mdMedia) {
     $scope.brewery = '';
     $rootScope.searchResults = [];
@@ -116,80 +115,139 @@ app.controller('UpdateCtrl', function($scope, $http, $timeout, $rootScope, $mdDi
     }
 
     $scope.updateDescription = function(ev, beer){
-        selectedBeer = beer;
-        console.log('selectedBeer is now' + JSON.stringify(selectedBeer));
-        console.log('trying to update the beer: ' + JSON.stringify(selectedBeer));
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
             $mdDialog.show({
-                controller: DescriptionController(beer, $scope),
+                controller: DescriptionController,
                 templateUrl: '../app/updatedialog.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose:true,
-                fullscreen: useFullScreen
-            });
-    }
-
-    $scope.submitDescription = function(ev){
-        console.log('the new description is ' + $scope.newDescription);
-        if ($scope.newDescription === ''){
-            // $scope.selectedBeer = {};
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .parent(angular.element(document.querySelector('#popupContainer')))
-                    .clickOutsideToClose(true)
-                    .textContent('Yo dawg, please provide a new description')
-                    .ariaLabel('Alert Dialog Demo')
-                    .ok('Got it!')
-                    .targetEvent(ev)
-            );
-        } else {
-            //TODO: modify t back to bname
-            console.log('selectedBeer is ' + JSON.stringify(selectedBeer));
-            var url = baseURL + '/beer/' + selectedBeer.name;
-            var payload = {'description':$scope.newDescription};
-
-            console.log('Making HTTP POST to ' + url + ' with a payload of ' + JSON.stringify(payload)); //!!!
-            $http({
-                method: 'POST',
-                url: url,
-                data: payload
-            }).then(function successCallback(response) {
-                console.log('received a response of ' + JSON.stringify(response.data));
-                if (response.data.updated === true){
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .parent(angular.element(document.querySelector('#popupContainer')))
-                            .clickOutsideToClose(true)
-                            .textContent('Successfully updated description.')
-                            .ariaLabel('Alert Dialog Demo')
-                            .ok('Got it!')
-                            .targetEvent(ev)
-                    );
-                } else {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .parent(angular.element(document.querySelector('#popupContainer')))
-                            .clickOutsideToClose(true)
-                            .textContent('Update failed.')
-                            .ariaLabel('Alert Dialog Demo')
-                            .ok('Got it!')
-                            .targetEvent(ev)
-                    );
+                fullscreen: useFullScreen,
+                locals:{
+                    beer: beer
                 }
-
-                // $scope.newDescription = '';
-                // $scope.selectedBeer = {};
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
             });
-        }
-
     }
+
+    function DescriptionController($scope, $mdDialog, beer){
+        $scope.selectedBeer = beer;
+        $scope.submitDescription = function(ev){
+            console.log('the new description is ' + $scope.newDescription);
+            if ($scope.newDescription === ''){
+                // $scope.selectedBeer = {};
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('#popupContainer')))
+                        .clickOutsideToClose(true)
+                        .textContent('Yo dawg, please provide a new description')
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('Got it!')
+                        .targetEvent(ev)
+                );
+            } else {
+                //TODO: modify t back to bname
+                console.log('$scope.selectedBeer is ' + JSON.stringify($scope.selectedBeer));
+                var url = baseURL + '/beer/' + $scope.selectedBeer.name;
+                var payload = {'description':$scope.newDescription};
+
+                console.log('Making HTTP POST to ' + url + ' with a payload of ' + JSON.stringify(payload)); //!!!
+                $http({
+                    method: 'POST',
+                    url: url,
+                    data: payload
+                }).then(function successCallback(response) {
+                    console.log('received a response of ' + JSON.stringify(response.data));
+                    if (response.data.updated === true){
+                        $mdDialog.show(
+                            $mdDialog.alert()
+                                .parent(angular.element(document.querySelector('#popupContainer')))
+                                .clickOutsideToClose(true)
+                                .textContent('Successfully updated description.')
+                                .ariaLabel('Alert Dialog Demo')
+                                .ok('Got it!')
+                                .targetEvent(ev)
+                        );
+                    } else {
+                        $mdDialog.show(
+                            $mdDialog.alert()
+                                .parent(angular.element(document.querySelector('#popupContainer')))
+                                .clickOutsideToClose(true)
+                                .textContent('Update failed.')
+                                .ariaLabel('Alert Dialog Demo')
+                                .ok('Got it!')
+                                .targetEvent(ev)
+                        );
+                    }
+
+                    // $scope.newDescription = '';
+                    // $scope.selectedBeer = {};
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+            }
+
+        }
+        
+    }
+
+    // $scope.submitDescription = function(ev){
+    //     console.log('the new description is ' + $scope.newDescription);
+    //     if ($scope.newDescription === ''){
+    //         // $scope.selectedBeer = {};
+    //         $mdDialog.show(
+    //             $mdDialog.alert()
+    //                 .parent(angular.element(document.querySelector('#popupContainer')))
+    //                 .clickOutsideToClose(true)
+    //                 .textContent('Yo dawg, please provide a new description')
+    //                 .ariaLabel('Alert Dialog Demo')
+    //                 .ok('Got it!')
+    //                 .targetEvent(ev)
+    //         );
+    //     } else {
+    //         //TODO: modify t back to bname
+    //         console.log('$scope.selectedBeer is ' + JSON.stringify($scope.selectedBeer));
+    //         var url = baseURL + '/beer/' + $scope.selectedBeer.bname;
+    //         var payload = {'description':$scope.newDescription};
+
+    //         console.log('Making HTTP POST to ' + url + ' with a payload of ' + JSON.stringify(payload)); //!!!
+    //         $http({
+    //             method: 'POST',
+    //             url: url,
+    //             data: payload
+    //         }).then(function successCallback(response) {
+    //             console.log('received a response of ' + JSON.stringify(response.data));
+    //             if (response.data.updated === true){
+    //                 $mdDialog.show(
+    //                     $mdDialog.alert()
+    //                         .parent(angular.element(document.querySelector('#popupContainer')))
+    //                         .clickOutsideToClose(true)
+    //                         .textContent('Successfully updated description.')
+    //                         .ariaLabel('Alert Dialog Demo')
+    //                         .ok('Got it!')
+    //                         .targetEvent(ev)
+    //                 );
+    //             } else {
+    //                 $mdDialog.show(
+    //                     $mdDialog.alert()
+    //                         .parent(angular.element(document.querySelector('#popupContainer')))
+    //                         .clickOutsideToClose(true)
+    //                         .textContent('Update failed.')
+    //                         .ariaLabel('Alert Dialog Demo')
+    //                         .ok('Got it!')
+    //                         .targetEvent(ev)
+    //                 );
+    //             }
+
+    //             // $scope.newDescription = '';
+    //             // $scope.selectedBeer = {};
+    //         }, function errorCallback(response) {
+    //             // called asynchronously if an error occurs
+    //             // or server returns response with an error status.
+    //         });
+    //     }
+
+    // }
 
 });
 
-function DescriptionController(beer, $scope){
-    $scope.selectedBeer = beer;
-}
