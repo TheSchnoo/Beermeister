4efcf4dcc40445004950b588d61dc2cef4bbf5b4
ReneeSlamka <renee.slamka@gmail.com>
2016-03-31 14:56:51 -0700

    Merged changes to web controller and added calls for signup and login

--- Brewmeister/src/main/java/web/WebController.java
+++ Brewmeister/src/main/java/web/WebController.java
@@ -1,10 +1,9 @@
 package web;
 
-import org.json.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,18 +14,17 @@ import java.util.Map;
 @Controller
 public class WebController {
 
-    @RequestMapping("/beers")
+    @RequestMapping(value = "/beers", method = RequestMethod.GET)
     public
     @ResponseBody
-//    String[] searchParams = new String[@RequestParam.split('=').length];
     ArrayList<BeerInfo> searchBeers (@RequestParam(value="bname", required = false) String bname,
                                      @RequestParam(value="type", required = false) String btype,
                                      @RequestParam(value="vendor", required = false) String vendor,
                                      @RequestParam(value="ibu", required = false) String ibu,
                                      @RequestParam(value="abv", required = false) String abv,
                                      @RequestParam(value="rating", required = false) String rating,
-//                                     @RequestParam(value="description", required = false) String description,
-                                     @RequestParam(value="breweryname", required = false) String breweryName,
+                                     @RequestParam(value="description", required = false) String description,
+                                     @RequestParam(value="breweryName", required = false) String breweryName,
                                      HttpServletResponse httpResponse) throws IOException {
 
         Map<String,String> searchBeerMap = new HashMap<>();
@@ -35,8 +33,8 @@ public class WebController {
         searchBeerMap.put("type", btype);
         searchBeerMap.put("ibu", ibu);
         searchBeerMap.put("abv", abv);
-        searchBeerMap.put("averageRating", rating);
-//        searchBeerMap.put("Description", description);
+        //searchBeerMap.put("averageRating", rating);
+        searchBeerMap.put("description", description);
         searchBeerMap.put("breweryName", breweryName);
 
         AccessDatabase accessDB = new AccessDatabase();
@@ -52,7 +50,7 @@ public class WebController {
         return beers;
     }
 
-    @RequestMapping("/recommendedbeers")
+    @RequestMapping(value = "/recommendedbeers", method = RequestMethod.GET)
     public
     @ResponseBody
     ArrayList<BeerInfo> recs (
@@ -105,4 +103,123 @@ public class WebController {
             httpResponse.setStatus(HttpServletResponse.SC_OK);
             return CustomerAccountService.logout(sessionId);
     }
+
+
+    @RequestMapping(value = "/reviews", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ArrayList<BeerInfo> revs (
+            @RequestParam(value="userid", required = false) String userid,
+            @RequestParam(value="bname", required = false) String bname,
+            HttpServletResponse httpResponse) throws IOException {
+        AccessDatabase accessDB = new AccessDatabase();
+        ArrayList<BeerInfo> reviews;
+
+        //TODO: ADD FUNCTIONALITY
+
+//        try {
+//            if(userid==null){
+//                //TODO search reviews by beer name
+//            }
+//            else{
+//                //TODO search reviews by a user
+//            }
+//            httpResponse.setStatus(HttpServletResponse.SC_OK);
+//        } catch (Exception e) {
+//            reviews = null;
+//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        }
+        return new ArrayList<BeerInfo>();
+    }
+
+    // Get vendors by beer name
+    @RequestMapping(value = "/vendors", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ArrayList<BeerInfo> vendors (
+            @RequestParam(value="bname", required = false) String bname,
+            HttpServletResponse httpResponse) throws IOException {
+        AccessDatabase accessDB = new AccessDatabase();
+        ArrayList<BeerInfo> reviews;
+
+        //TODO: ADD FUNCTIONALITY FOR GETTING VENDORS THAT SELL A BEER
+
+//        try {
+//            httpResponse.setStatus(HttpServletResponse.SC_OK);
+//        } catch (Exception e) {
+//            reviews = null;
+//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        }
+        return new ArrayList<BeerInfo>();
+    }
+
+
+//    POST REQUESTS
+//++++++++++++++++++++++++++++++++++
+    @RequestMapping(value = "/beers", method = RequestMethod.POST)
+    public @ResponseBody JSONObject addBeer(@RequestBody String body) throws JSONException {
+        JSONObject bodyJSON = new JSONObject(body);
+        String bname = bodyJSON.getString("bname");
+        String breweryName = bodyJSON.getString("breweryName");
+        String type = bodyJSON.getString("type");
+        Double abv = bodyJSON.getDouble("abv");
+        Double ibu = bodyJSON.getDouble("ibu");
+        String description = bodyJSON.getString("description");
+        Boolean brewed = bodyJSON.getBoolean("brewed");
+
+        BeerInfo newBI = new BeerInfo(bname, breweryName, type, abv, ibu, description, brewed);
+
+        AccessDatabase accessDB = new AccessDatabase();
+
+        try {
+            accessDB.addBeer(newBI);
+        } catch (Exception e) {
+            return new JSONObject().append("created", false);
+        }
+
+        return new JSONObject().append("created", true);
+    }
+//    ArrayList<BeerInfo> beers (
+//            @RequestParam(value="bname", required = false) String bname,
+//            HttpServletResponse httpResponse) throws IOException {
+//        AccessDatabase accessDB = new AccessDatabase();
+//        ArrayList<BeerInfo> reviews;
+//
+//        try {
+//            httpResponse.setStatus(HttpServletResponse.SC_OK);
+//        } catch (Exception e) {
+//            reviews = null;
+//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        }
+//        return new ArrayList<BeerInfo>();
+//    }
+
+    @RequestMapping(value = "/beers/update", method = RequestMethod.POST)
+    public @ResponseBody JSONObject updateBeer(@RequestBody String body, @RequestParam(value="bname") String bname) throws JSONException {
+
+        JSONObject bodyJSON = new JSONObject(body);
+        AccessDatabase accessDB = new AccessDatabase();
+
+        try {
+            accessDB.updateBeerToDB(bname, bodyJSON);
+        } catch (Exception e) {
+            return new JSONObject().append("updated", false);
+        }
+
+        return new JSONObject().append("updated", true);
+    }
+//    ArrayList<BeerInfo> beers (
+//            @RequestParam(value="bname", required = false) String bname,
+//            HttpServletResponse httpResponse) throws IOException {
+//        AccessDatabase accessDB = new AccessDatabase();
+//        ArrayList<BeerInfo> reviews;
+//
+//        try {
+//            httpResponse.setStatus(HttpServletResponse.SC_OK);
+//        } catch (Exception e) {
+//            reviews = null;
+//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        }
+//        return new ArrayList<BeerInfo>();
+//    }
 }
