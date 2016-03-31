96c389245ebbbffaabf16e2689256b5fcdd90d45
TheSchnoo <wes.berry1@gmail.com>
2016-03-31 16:39:15 -0700

    Updated /beers endpoint.

--- Brewmeister/src/main/java/web/WebController.java
+++ Brewmeister/src/main/java/web/WebController.java
@@ -120,28 +120,45 @@ public class WebController {
 //    POST REQUESTS
 //++++++++++++++++++++++++++++++++++
     @RequestMapping(value = "/beers", method = RequestMethod.POST)
-    public @ResponseBody JSONObject addBeer(@RequestBody String body) throws JSONException {
-        JSONObject bodyJSON = new JSONObject(body);
-        String bname = bodyJSON.getString("bname");
-        String breweryName = bodyJSON.getString("breweryName");
-        String type = bodyJSON.getString("type");
-        Double abv = bodyJSON.getDouble("abv");
-        Double ibu = bodyJSON.getDouble("ibu");
-        String description = bodyJSON.getString("description");
-        Boolean brewed = bodyJSON.getBoolean("brewed");
+    public @ResponseBody JSONObject postBeer(@RequestBody String body,
+                                             @RequestParam(value="bname", required=false) String beerName) throws JSONException {
+        // Add a beer
+        if(beerName == null){
+            JSONObject bodyJSON = new JSONObject(body);
+            String bname = bodyJSON.getString("bname");
+            String breweryName = bodyJSON.getString("breweryName");
+            String type = bodyJSON.getString("type");
+            Double abv = bodyJSON.getDouble("abv");
+            Double ibu = bodyJSON.getDouble("ibu");
+            String description = bodyJSON.getString("description");
+            Boolean brewed = bodyJSON.getBoolean("brewed");
 
-        BeerInfo newBI = new BeerInfo(bname, breweryName, type, abv, ibu, description, brewed);
+            BeerInfo newBI = new BeerInfo(bname, breweryName, type, abv, ibu, description, brewed);
 
-        AccessDatabase accessDB = new AccessDatabase();
+            AccessDatabase accessDB = new AccessDatabase();
 
-        try {
-            accessDB.addBeer(newBI);
-        } catch (Exception e) {
-            return new JSONObject().append("created", false);
+            try {
+                accessDB.addBeer(newBI);
+            } catch (Exception e) {
+                return new JSONObject().append("created", false);
+            }
+
+            return new JSONObject().append("created", true);
         }
 
-        return new JSONObject().append("created", true);
-    }
+        // Update a beer
+        else {
+            JSONObject bodyJSON = new JSONObject(body);
+            AccessDatabase accessDB = new AccessDatabase();
+
+            try {
+//                accessDB.updateBeerToDB(beerName, bodyJSON);
+            } catch (Exception e) {
+                return new JSONObject().append("updated", false);
+            }
+
+            return new JSONObject().append("updated", true);
+        }
 //    ArrayList<BeerInfo> beers (
 //            @RequestParam(value="bname", required = false) String bname,
 //            HttpServletResponse httpResponse) throws IOException {
@@ -157,35 +174,6 @@ public class WebController {
 //        return new ArrayList<BeerInfo>();
 //    }
 
-    @RequestMapping(value = "/beers/update", method = RequestMethod.POST)
-    public @ResponseBody JSONObject updateBeer(@RequestBody String body, @RequestParam(value="bname") String bname) throws JSONException {
-
-        JSONObject bodyJSON = new JSONObject(body);
-        AccessDatabase accessDB = new AccessDatabase();
-
-        try {
-            accessDB.updateBeerToDB(bname, bodyJSON);
-        } catch (Exception e) {
-            return new JSONObject().append("updated", false);
-        }
-
-        return new JSONObject().append("updated", true);
     }
-//    ArrayList<BeerInfo> beers (
-//            @RequestParam(value="bname", required = false) String bname,
-//            HttpServletResponse httpResponse) throws IOException {
-//        AccessDatabase accessDB = new AccessDatabase();
-//        ArrayList<BeerInfo> reviews;
-//
-//        try {
-//            httpResponse.setStatus(HttpServletResponse.SC_OK);
-//        } catch (Exception e) {
-//            reviews = null;
-//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//        }
-//        return new ArrayList<BeerInfo>();
-//    }
-
-
 
 }
