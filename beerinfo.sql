3d02da7179503d26fc6afdc52945c13d7522257d
Paul Dante <pdante@Paul-Desky>
2016-03-31 00:13:05 -0700

    Added Avg field to BeerInfo table, added Average Ratings table, created a procedure that updates the average rating for a beer, and added a trigger that automatically calls the update function upon the addition of a new rating.

--- beerinfo.sql
+++ beerinfo.sql
@@ -18,10 +18,14 @@ CREATE TABLE BeerInfo (
 	IBU FLOAT,
 	ABV FLOAT,
 	Description CHAR(255),
-	BreweryName CHAR(30),	
+	BreweryName CHAR(30),
+	Avg FLOAT NOT NULL ,
 	PRIMARY KEY(BName),
 	-- CANDIDATE KEY (FName),
 	FOREIGN KEY(BreweryName) REFERENCES Brewery (bname)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION,
+	FOREIGN KEY(Avg) REFERENCES Ratings (Avg_rating)
 		ON UPDATE CASCADE
 		ON DELETE NO ACTION
 );
@@ -62,6 +66,7 @@ CREATE TABLE StoreEmployeeHasA (
 CREATE TABLE Rates (
 	CID INTEGER,
 	BName CHAR(30),
+	BRate INTEGER,
 	PRIMARY KEY (CID, BName),
 	FOREIGN KEY (CID) REFERENCES Customer (CID),
 	FOREIGN KEY (BName) REFERENCES BeerInfo (BName)
@@ -100,7 +105,18 @@ CREATE TABLE Searches(
 	FOREIGN KEY (BName) REFERENCES BeerInfo (BName)
 );
 
+CREATE TABLE Ratings(
+	Bname CHAR(30),
+	Avg_Rating FLOAT,
+	PRIMARY KEY(Bname),
+	FOREIGN KEY (BName) REFERENCES BeerInfo (BName)
+);
+
+CREATE TRIGGER the_averages AFTER INSERT ON Rates
+FOR EACH ROW CALL update_or_insert(BName,BRate);
+
 -- grant select on Searches to public;
+
 
 insert into BeerInStock
 values('Gypsy Tears',0);
@@ -118,35 +134,47 @@ insert into BeerVendor
 values(3, 'Darby\'s Liquor Store');
 
 insert into BeerInfo
-values('Gypsy Tears', 'Ruby Ale', 'GT Profile', 40, 6.0, 'Caramel', 'Parallel 49');
+values('Gypsy Tears', 'Ruby Ale', 'GT Profile', 40, 6.0, 'Caramel', 'Parallel 49',0);
 
 insert into Brewery
 values('Parallel 49');
 
 insert into BeerInfo
-values('Watermelon Witbier', 'Hefeweizen', 'WW Profile', 50, 5.22, 'Fruity, refreshing', 'Parallel 49');
+values('Watermelon Witbier', 'Hefeweizen', 'WW Profile', 50, 5.22, 'Fruity, refreshing', 'Parallel 49',0);
 
 insert into BeerInfo
-values('Jerkface 9000', 'Wheat Ale', 'JF Profile', 37, 5.0, 'Citrus, floral, malt base, hop punch', 'Parallel 49');
+values('Jerkface 9000', 'Wheat Ale', 'JF Profile', 37, 5.0, 'Citrus, floral, malt base, hop punch', 'Parallel 49',0);
 
 -- Procedure;
+DELIMITER $$
 
-CREATE PROCEDURE update_average
-	(IN beername CHAR(30),IN beeravg FLOAT)
+Create PROCEDURE update_or_insert
+	(IN beername CHAR(30), IN rate INTEGER)
+	MODIFIES SQL DATA
 	BEGIN
-		UPDATE BEERINFO
-		SET avg = beeravg
-		WHERE beername = BName;
-	END;
+		DECLARE duplicate_key INT DEFAULT 0;
+		BEGIN
+			DECLARE EXIT HANDLER FOR 1062 SET duplicate_key = 1;
+			INSERT into Ratings (Bname, Avg_Rating) VALUES (beername,rate);
+
+		END;
+
+	IF duplicate_key=1 THEN
+		CALL update_avg_ratings_table(beername,rate);
+	END IF;
+END $$
+
+
 
 CREATE PROCEDURE update_avg_ratings_table
-	(INOUT beername CHAR(30), OUT average FLOAT)
+	(IN beername CHAR(30))
 	BEGIN
-		UPDATE RatingsTable
-		SET average = (SELECT AVG(rating)
+		UPDATE Ratings
+		SET Avg_Rating = (SELECT AVG(Rates.BRate)
 									 FROM Ratings
 									 WHERE BName=beername)
 		WHERE beername = BName;
-	END;
+	END $$
+
 
 -- example of the above 2 procedures in use update_average(update_avg_ratings_table("Parallel 49"));
