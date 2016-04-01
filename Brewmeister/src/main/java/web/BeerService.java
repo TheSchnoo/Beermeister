0b6a52efd13759fe57d680a59745dac4082a34e7
TheSchnoo <wes.berry1@gmail.com>
2016-03-31 22:30:35 -0700

    Refactored backend beer and vendor access to database

---
+++ Brewmeister/src/main/java/web/BeerService.java
@@ -0,0 +1,109 @@
+package web;
+
+import org.json.JSONObject;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class BeerService {
+
+    public String getBeers(Map<String, String> searchBeerMap) throws Exception {
+        String searchString;
+        searchString = "WHERE ";
+        int i=0;
+        for(Map.Entry<String,String> entry : searchBeerMap.entrySet()){
+            if(entry.getValue() == null){
+                continue;
+            }
+            if(i>0){
+                searchString = searchString + " AND ";
+            }
+            if(entry.getKey()=="ibu"){
+                float value = Float.parseFloat(entry.getValue());
+                if(value < 9){
+                    searchString = searchString + entry.getKey() + " < " + 9;
+                }
+                else if(value >= 80){
+                    searchString = searchString + entry.getKey() + " >= " + 80;
+                }
+                else {
+                    searchString = searchString + entry.getKey() + " BETWEEN " + value + " AND " + (value + (float) 9);
+                }
+            }
+            else if(entry.getKey()=="abv"){
+                float value = Float.parseFloat(entry.getValue());
+                float upperRange = value + (float) 0.99;
+                if(value < 4){
+                    searchString = searchString + entry.getKey() + " < " + 4;
+                }
+                else if(value >= 7){
+                    searchString = searchString + entry.getKey() + " >= " + 7;
+                }
+                else{
+                    searchString = searchString + entry.getKey() + " BETWEEN " + value + " AND " + upperRange;
+                }
+            }
+            else {
+                searchString = searchString + entry.getKey() + " LIKE " + "'%" + entry.getValue() + "%'";
+            }
+            i++;
+        }
+        if(i==0){
+            searchString = "";
+        }
+        System.out.println(searchString);
+
+        return searchString;
+    }
+
+    public String getRecommendations(int userid) throws Exception {
+        String searchString = "";
+            return searchString;
+    }
+
+    public Map<String, String> updateBeerToDB(String bname, JSONObject jobj) throws Exception {
+        Map searchMap = new TreeMap<String, String>();
+        if(jobj.getString("Description")!=null){
+            searchMap.put("Description", jobj.getString("Description"));
+        }
+        if(jobj.getString("Brewed") != null){
+            searchMap.put("Brewed",jobj.getString("Brewed"));
+        }
+        searchMap.put("BName", bname);
+
+        return searchMap;
+    }
+
+    // Convert a ResultSet to a BeerInfo object
+    public BeerInfo convertResultSetToBeerInfo(ResultSet rs){
+
+        VendorService vs = new VendorService();
+
+        try{
+            String bname = rs.getString("BName");
+            String breweryName = rs.getString("BreweryName");
+            String type = rs.getString("Type");
+            float abv = rs.getFloat("ABV");
+            float ibu = rs.getFloat("IBU");
+            String description = rs.getString("Description");
+//            Boolean brewed = rs.getBoolean("Brewed");
+//            String averageRating; = rs.getString("")
+//		  String imageLocation;
+
+            ArrayList<Vendor> vendors = vs.getVendorsThatSellABeer(bname);
+
+
+            BeerInfo newBI = new BeerInfo(bname, breweryName, type, abv, ibu, description, true, vendors);
+
+//            return obj;
+            return newBI;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+}
--- Brewmeister/src/main/java/web/WebController.java
+++ Brewmeister/src/main/java/web/WebController.java
@@ -1,5 +1,6 @@
 package web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class WebController {
@@ -29,6 +31,7 @@ public class WebController {
                                      @RequestParam(value="storeName", required = false) String storeName,
                                      HttpServletResponse httpResponse) throws IOException {
         ArrayList<BeerInfo> beers;
+        AccessDatabase accessDatabase = new AccessDatabase();
 
         if(storeName == null) {
 
@@ -42,9 +45,9 @@ public class WebController {
             searchBeerMap.put("description", description);
             searchBeerMap.put("breweryName", breweryName);
 
-            AccessDatabase accessDB = new AccessDatabase();
+            BeerService beerService = new BeerService();
             try {
-                beers = accessDB.searchBeers(searchBeerMap);
+                beers = accessDatabase.searchBeers(beerService.getBeers(searchBeerMap));
             } catch (Exception e) {
                 beers = null;
             }
@@ -52,9 +55,9 @@ public class WebController {
 
         }
         else{
-            VendorService vendorService = new VendorService();
             try {
-                beers = vendorService.getBeersByVendor(storeName);
+                VendorService vendorService = new VendorService();
+                beers = accessDatabase.searchBeers(vendorService.getBeersByVendor(storeName));
             } catch (Exception e) {
                 beers = null;
                 e.printStackTrace();
@@ -177,31 +180,25 @@ public class WebController {
 
         // Update a beer
         else {
+            HashMap<String,String> updateMap;
             JSONObject bodyJSON = new JSONObject(body);
+            try {
+                updateMap = new ObjectMapper().readValue(bodyJSON.toString(), HashMap.class);
+            } catch (IOException e) {
+                updateMap = null;
+                e.printStackTrace();
+            }
             AccessDatabase accessDB = new AccessDatabase();
 
             try {
-//                accessDB.updateBeerToDB(beerName, bodyJSON);
+                beerName = "beerName=" + beerName;
+                accessDB.updateToDB("BeerInfo", updateMap, beerName);
             } catch (Exception e) {
                 return new JSONObject().append("updated", false);
             }
 
             return new JSONObject().append("updated", true);
         }
-//    ArrayList<BeerInfo> beers (
-//            @RequestParam(value="bname", required = false) String bname,
-//            HttpServletResponse httpResponse) throws IOException {
-//        AccessDatabase accessDB = new AccessDatabase();
-//        ArrayList<BeerInfo> reviews;
-//
-//        try {
-//            httpResponse.setStatus(HttpServletResponse.SC_OK);
-//        } catch (Exception e) {
-//            reviews = null;
-//            httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//        }
-//        return new ArrayList<BeerInfo>();
-//    }
 
     }
 }
